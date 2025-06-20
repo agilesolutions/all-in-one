b745affa99adededb7609dc054c04b036ae2b030
admin <rong@agile-solutions.ch>
2025-06-20 14:17:02 +0200

    restclient

--- src/main/java/com/agilesolutions/controller/StockController.java
+++ src/main/java/com/agilesolutions/controller/StockController.java
@@ -4,21 +4,20 @@ import com.agilesolutions.dto.StockResponse;
 import com.agilesolutions.service.StockService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/data")
 @AllArgsConstructor
 public class StockController {
 
     private final StockService stockService;
 
-    @GetMapping("/stockPrices")
-    StockResponse getLatestStockPrices(@PathVariable String company) {
+    @GetMapping(value = "/stockPrices/{company}", produces = MediaType.APPLICATION_JSON_VALUE)
+    StockResponse getLatestStockPrices(@PathVariable("company") String company) {
 
         log.info("Get stock prices for: {}", company);
         return stockService.getLatestStockPrices(company);
--- src/test/java/com/agilesolutions/controller/StockControllerTest.java
+++ src/test/java/com/agilesolutions/controller/StockControllerTest.java
@@ -3,21 +3,28 @@ package com.agilesolutions.controller;
 import com.agilesolutions.config.ApplicationProperties;
 import com.agilesolutions.config.JunitConfig;
 import com.agilesolutions.dto.StockResponse;
+import com.agilesolutions.exception.CustomControllerAdvice;
 import com.agilesolutions.service.StockService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.concurrent.TimeoutException;
+
+import static org.hamcrest.Matchers.containsStringIgnoringCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(StockController.class)
-@ContextConfiguration(classes = {JunitConfig.class, ApplicationProperties.class})
+@ContextConfiguration(classes = {StockController.class, JunitConfig.class, ApplicationProperties.class, CustomControllerAdvice.class})
 class StockControllerTest {
 
     @Autowired
@@ -29,7 +36,6 @@ class StockControllerTest {
     @Autowired
     private ApplicationProperties applicationProperties;
 
-
     @Test
     public void givenFinancialAssets_whenInquiringApple_thenReturnStockPricesForApple() throws Exception {
 
@@ -37,10 +43,22 @@ class StockControllerTest {
         Mockito.when(stockService.getLatestStockPrices("APPL")).thenReturn(StockResponse.builder().price(1.0F).build());
 
         // THEN
-        mockMvc.perform(get("/data/stockPrices").param("company", "AAPL")
-                        .contentType(MediaType.APPLICATION_JSON))
-                //.andExpect(status().isOk())
-                .andDo(print());
-        //.andExpect(content().string(containsStringIgnoringCase("cat")));
+        ResultActions resultActions = mockMvc.perform(get("/stockPrices/{company}", "APPL"))
+                .andExpect(status().isOk())
+                //.andDo(print());
+                .andExpect(content().string(containsStringIgnoringCase("1.0")));
+    }
+
+    @Test
+    public void givenResourceNotAvailable_whenGetSpecificException_thenTimeOut() throws Exception {
+
+        // WHEN
+        Mockito.when(stockService.getLatestStockPrices("NONE")).thenThrow(new IllegalStateException("Resource not Available"));
+
+
+        mockMvc.perform(get("/stockPrices/{company}", "NONE"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof IllegalStateException))
+                .andExpect(result -> assertEquals("Resource not Available", result.getResolvedException().getMessage()));
     }
 }
