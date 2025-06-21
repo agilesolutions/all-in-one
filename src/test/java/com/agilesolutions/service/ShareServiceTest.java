f019fa471101e47cb2ba6661406cd65cfcc7ce7d
admin <rong@agile-solutions.ch>
2025-06-21 12:41:45 +0200

    data jpa

---
+++ src/test/java/com/agilesolutions/service/ShareServiceTest.java
@@ -0,0 +1,7 @@
+package com.agilesolutions.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShareServiceTest {
+
+}
---
+++ src/test/java/com/base/BaseIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.base;
+
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class BaseIntegrationTest {
+    static int containerPort = 5432;
+    static int localPort = 5432;
+
+    @ServiceConnection
+    protected static final PostgreSQLContainer<?> dbContainer = new PostgreSQLContainer<>(
+            DockerImageName.parse("postgres")
+                    .withTag("latest"))
+            .withReuse(true)
+            .withExposedPorts(containerPort)
+            .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
+                    new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(localPort), new ExposedPort(containerPort)))
+
+            ));
+
+    static {
+        dbContainer.setCommand("postgres","-c","max_connections=2000");
+        dbContainer.start();
+    }
+
+}
