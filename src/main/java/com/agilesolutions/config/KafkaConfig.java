47a7175af1444c49a32bda1a995d92e7b53d98e8
admin <rong@agile-solutions.ch>
2025-06-24 14:57:48 +0200

    kafka

--- src/main/java/com/agilesolutions/config/KafkaConfig.java
+++ src/main/java/com/agilesolutions/config/KafkaConfig.java
@@ -1,12 +1,17 @@
 package com.agilesolutions.config;
 
+import com.agilesolutions.dto.Share;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,23 +23,34 @@ public class KafkaConfig {
     private String bootstrapServers;
 
     @Bean
-    public Map<String, Object> producerConfigs() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, AvroSerializer.class);
+    public ConsumerFactory<String, Share> consumerFactory() {
 
-        return props;
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "default-task-group");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        configProps.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+        configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        return new DefaultKafkaConsumerFactory<>(configProps);
     }
 
     @Bean
-    public ProducerFactory<String, User> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    public ProducerFactory<String, Share> producerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
     public KafkaTemplate<String, Share> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+        var kafkaTemplate = new KafkaTemplate<>(producerFactory());
+        kafkaTemplate.setConsumerFactory(consumerFactory());
+        kafkaTemplate.setDefaultTopic("default-task-group");
+        return kafkaTemplate;
     }
 
 }
---
+++ src/main/java/com/agilesolutions/init/KafkaInitializer.java
@@ -0,0 +1,34 @@
+package com.agilesolutions.init;
+
+
+import com.agilesolutions.dto.Share;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+@Slf4j
+public class KafkaInitializer implements CommandLineRunner {
+
+    @Autowired
+    private KafkaTemplate<String, Share> kafkaTemplate;
+
+
+
+    @Override
+    public void run(String... args) {
+
+        Share.newBuilder().setCompany("AAPL").setId(1).setQuantity(100).setUpdated(Instant.now());
+
+        kafkaTemplate.send()
+
+        log.info("Kafka records saved successfully-------");
+
+    }
+}
