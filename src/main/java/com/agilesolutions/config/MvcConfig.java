bbad8b1a15cbb36e219a2170c020cd2a3fcbc0fd
admin <rong@agile-solutions.ch>
2025-06-25 16:48:14 +0200

    kafka

--- src/main/java/com/agilesolutions/config/MvcConfig.java
+++ src/main/java/com/agilesolutions/config/MvcConfig.java
@@ -7,13 +7,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
-public class ConverterConfig extends WebMvcConfigurerAdapter {
+@EnableWebMvc
+public class MvcConfig  implements WebMvcConfigurer {
 
-    @Override
+
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        super.configureMessageConverters(converters);
         converters.add(new AvroJsonHttpMessageConverter<SpecificRecordBase>());
     }
 
